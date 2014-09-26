00d490b325c303dc89dae5a52ed4f4bbc1800f09
Andrew Tredennick <atredenn@Andrews-iMac.local>
2014-09-26 15:01:07 -0600

    added competition sensitivity IPM and calculation

---
+++ PopModels/idahoIPM/multispp_glm_v4/simulations/calcCompSensitivity.R
@@ -0,0 +1,16 @@
+#Estimate sensitivity to competition for each species
+
+#Sensitivity to competition: S = 1 - (r[invasion]-r[alone])
+
+invadeD <- read.csv("invasionGrowthRates_YrFluct.csv")
+intrinsicD <- read.csv("intrinsicGrowthRates_YrFluct.csv")
+
+S <- 1 - (invadeD$maxR/intrinsicD$maxR)
+S <- as.data.frame(S)
+S$species <- invadeD$species
+
+library(ggplot2)
+ggplot(S, aes(x=species, y=S))+
+  geom_bar(stat="identity")+
+  ylab("Sensitivity to Competition (S)")
+  theme_bw()
