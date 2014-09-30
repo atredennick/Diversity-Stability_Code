54776971b88295019c706e0a4abf282abee7e06b
Andrew Tredennick <atredenn@Andrews-iMac.local>
2014-09-30 15:00:52 -0600

    checked for influence of neg pgr

--- PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
+++ PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
@@ -6,11 +6,14 @@ library(ggplot2)
 
 cv <- read.csv("CV_rVary.csv")
 sdr <- read.csv("rSD_rVary.csv")
+pgrs <- read.csv("pgrs_rVary.csv")
+
+tmpI <- which(pgrs<0, arr.ind = TRUE)[,1]
 
 plotD <- as.data.frame(cbind(cv[,2], sdr[,2]))
 colnames(plotD) <- c("cv", "sdr")
 
-ggplot(plotD, aes(x=sdr, y=cv))+
+ggplot(plotD[-tmpI,], aes(x=sdr, y=cv))+
   geom_point(shape=19, size=3)+
   geom_smooth(color="purple", size=1, fill="purple", method="loess")+
   theme_bw()+
