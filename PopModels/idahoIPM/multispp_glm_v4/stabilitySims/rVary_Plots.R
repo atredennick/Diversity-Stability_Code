16cb46df245d9e8bce4de794d893dc8d09b710a1
Andrew Tredennick <atredenn@gmail.com>
2014-10-28 10:30:30 -0600

    updated with plot for average r

--- PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
+++ PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
@@ -15,10 +15,10 @@ for(i in 3:5){
 
 tmpI <- which(pgrs<0, arr.ind = TRUE)[,1]
 
-plotD <- as.data.frame(cbind(cv[,2], sdr[,2]))
-colnames(plotD) <- c("cv", "sdr")
+plotD <- as.data.frame(cbind(cv[,2], sdr[,2], apply(pgrs[,2:5], MARGIN = 1, FUN = mean)))
+colnames(plotD) <- c("cv", "sdr", "meanR")
 
-ggplot(plotD[-tmpI,], aes(x=sdr, y=cv))+
+ggplot(plotD[-tmpI,], aes(x=meanR, y=cv))+
   geom_point(shape=19, size=3)+
   geom_smooth(color="purple", size=1, fill="purple", method="loess")+
   theme_bw()+
