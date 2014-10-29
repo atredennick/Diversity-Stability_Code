b2a08dfa85e35503692e88ece1579dac25344b02
Andrew Tredennick <atredenn@gmail.com>
2014-10-29 14:22:32 -0600

    updated output; likely wrong though

--- PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
+++ PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_Plots.R
@@ -2,11 +2,22 @@
 #### PLOT RESULTS FROM VARYING-r SIMULATIONS
 ####
 
+
+
 library(ggplot2)
 
 cv <- read.csv("CV_rVary.csv")
 sdr <- read.csv("rSD_rVary.csv")
 pgrs <- read.csv("pgrs_rVary.csv")
+monoSynch <- read.csv("monoSynch.csv")
+commSynch <- read.csv("commSynchrony_rVary.csv")
+
+plot(monoSynch[,2], commSynch[,2])
+mod <- lm(monoSynch[,2]~commSynch[,2])
+resmod <- resid(mod)
+
+plot(resmod, cv[,2])
+plot(commSynch[,2], cv[,2])
 
 plot(density(pgrs[,2], adjust=5), col=1, lwd=3)
 for(i in 3:5){
@@ -18,7 +29,7 @@ tmpI <- which(pgrs<0, arr.ind = TRUE)[,1]
 plotD <- as.data.frame(cbind(cv[,2], sdr[,2], apply(pgrs[,2:5], MARGIN = 1, FUN = mean)))
 colnames(plotD) <- c("cv", "sdr", "meanR")
 
-ggplot(plotD[-tmpI,], aes(x=meanR, y=cv))+
+ggplot(plotD[-tmpI,], aes(x=sdr, y=cv))+
   geom_point(shape=19, size=3)+
   geom_smooth(color="purple", size=1, fill="purple", method="loess")+
   theme_bw()+
