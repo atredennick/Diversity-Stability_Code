dba8f8c18b4aed1821f6cddca0f845cbe2b07832
Andrew Tredennick <atredenn@gmail.com>
2015-01-09 17:01:07 -0700

    pgr intrinsic results

--- PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_pgrWrapper.R
+++ PopModels/idahoIPM/multispp_glm_v4/stabilitySims/rVary_pgrWrapper.R
@@ -2,7 +2,7 @@
 #### Wrapper for intrinsic and invasion growth rates
 ####
 
-setwd("~/Repos/Diversity_Stability/PopModels/idahoIPM/multispp_glm_v4/stabilitySims")
+setwd("C:/Users/atredenn/Repos/Diversity_Stability/PopModels/idahoIPM/multispp_glm_v4/stabilitySims")
 source("ipm_noOverlap_rVary_intrinsic.R")
-setwd("~/Repos/Diversity_Stability/PopModels/idahoIPM/multispp_glm_v4/stabilitySims")
+setwd("C:/Users/atredenn/Repos/Diversity_Stability/PopModels/idahoIPM/multispp_glm_v4/stabilitySims")
 source("ipm_noOverlap_rVary_invasion.R")
