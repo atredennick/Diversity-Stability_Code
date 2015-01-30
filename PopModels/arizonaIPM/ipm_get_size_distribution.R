fce3c69ad02b6b1606338674f27e8709b2c678ec
Andrew Tredennick <atredenn@gmail.com>
2015-01-30 13:31:05 -0700

    initial commit of generic imp for size dists

---
+++ PopModels/arizonaIPM/ipm_get_size_distribution.R
@@ -0,0 +1,25 @@
+###############################################################
+#### Script to get stable size distribution for Arizona
+#### species from IPM simulation.
+####
+#### Andrew Tredennick: atredenn@gmail.com
+#### 1-30-2015
+
+####
+#### Clear the workspace -----------------------
+####
+rm(list=ls(all=TRUE))
+
+####
+#### Load IPMdoit library ----------------------
+####
+library(IPMdoit)
+
+####
+#### Bring in data -----------------------------
+####
+
+
+
+
+
