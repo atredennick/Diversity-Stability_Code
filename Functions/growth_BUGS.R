7fdfd79a4f8225feec43f3c107f2bd46efc5ac6b
Andrew Tredennick <atredenn@gmail.com>
2015-01-30 13:30:50 -0700

    initial commit of BUGS function for growth
    For NIMBLE

---
+++ Functions/growth_BUGS.R
@@ -0,0 +1,35 @@
+##############################################
+#### Vital rate regression for growth.
+####
+#### Andrew Tredennick: atredenn@gmail.com
+#### 1-30-2015
+
+####
+#### Write BUGS code for NIMBLE
+####
+g_code <- ({
+  for(i in 1:totN){
+    logsize1[i]~dnorm(mu[i],tau3[i])
+    mu[i]<-intcpt.yr[Yr[i]]+Gbeta[G[i]]+slope.yr[Yr[i]]*logsize0[i]+inprod(NBbeta.mu[1:Nspp],crowd[i,])  
+    tau2[i]<-1/(tau*exp(tauSize*mu[i]))  ##I like this, similar with ones from glm
+    tau3[i]<-max(tau2[i],0.00000001)  
+  }
+  tau~dnorm(0,0.001)
+  tauSize~dnorm(0,0.001)
+  for(j in 1:Ngroups){
+    Gbeta[j]~dnorm(0,tauGroup) ##using hyperprior here
+  }
+  tauGroup~dgamma(0.001,0.001)
+  intcpt.mu~dnorm(0,0.001)
+  slope.mu~dnorm(0,0.001)
+  intcpt.tau~dgamma(0.5,0.5)
+  slope.tau~dgamma(0.5,0.5)
+  for(k in 1:Nyears){
+    slope.yr[k]~dnorm(slope.mu,slope.tau)
+    intcpt.yr[k]~dnorm(intcpt.mu,intcpt.tau)
+  }
+  for(m in 1:Nspp){
+    NBbeta.mu[m]~dnorm(0,NBbeta.tau)I(-2,-0.00001) ##change this to meet BUGS(too wide to fail)
+  }
+  NBbeta.tau~dgamma(0.5,0.5)
+})
