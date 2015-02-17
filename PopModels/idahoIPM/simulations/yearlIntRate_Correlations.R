4e70b9324027ac6aa1a9c660264683a2d6cb9027
Andrew Tredennick <atredenn@gmail.com>
2015-02-17 10:55:31 -0700

    updated scripts for new mexico

--- PopModels/idahoIPM/simulations/yearlIntRate_Correlations.R
+++ PopModels/idahoIPM/simulations/yearlIntRate_Correlations.R
@@ -7,9 +7,15 @@ library(reshape2)
 library(ggplot2)
 library(reshape2)
 library(synchrony)
+library(plyr)
 
 cbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
                "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+#Get quadrat group information
+quad_ids <- read.csv("../../../Data/Idaho/ARTR/growDnoNA.csv")
+quad_groups <- unique(quad_ids[,c("quad","Group")])
+quad_groups$Group <- as.numeric(quad_groups$Group)
 
 all_data <- readRDS("RmaxYearly_byGroup_ipm.rds")
 ts_metrics <- readRDS("../../../EmpiricalRelationships/quad_ts_metrics_AllSites.rds")
@@ -19,23 +25,34 @@ for(i in 1:6){
   tmp <- test[,c(i,i+6,i+12,i+18)]
   synch[i] <- community.sync(tmp)[[1]]
 }
+synch <- as.data.frame(synch)
+synch$Group <- c(1:6)
+colnames(synch)[1] <- "yr_synch"
+synch <- merge(synch, quad_groups, by="Group")
 
-test$year <- seq(1,22)
-testM <- melt(test, id.vars = "year")
-testM$species <- c(rep("ARTR", 22*6),
-                   rep("HECO", 22*6),
-                   rep("POSE", 22*6),
-                   rep("PSSP", 22*6))
-for(i in 1:4){
-  tmp <- testM
-}
+# test$year <- seq(1,22)
+# testM <- melt(test, id.vars = "year")
+# testM$species <- c(rep("ARTR", 22*6),
+#                    rep("HECO", 22*6),
+#                    rep("POSE", 22*6),
+#                    rep("PSSP", 22*6))
+# testM$Group <- rep(c(1:6),each=length(unique(testM$year)))
+# test_all <- merge(testM, quad_groups, by="Group")
 
+ts_df <- as.data.frame(cbind(ts_metrics$Idaho$quad, ts_metrics$Idaho$stability))
+colnames(ts_df) <- c("quad", "stability")
+ts_synch <- merge(synch, ts_df, by="quad")
+ts_out <- ddply(ts_synch, .(Group), summarise,
+                stability = mean(as.numeric(stability)),
+                yr_synch = mean(yr_synch))
+ggplot(ts_out, aes(x=yr_synch, y=log(stability)))+
+  geom_point(size=4)
 
 
 allD <- read.csv("RmaxYearly_ipm.csv")
 df <- melt(allD, id.vars = "yearParams")
 
-synch <- community.sync(allD[,1:4])[[1]]
+synch_yr <- community.sync(allD[,1:4])[[1]]
 ggplot(df, aes(x=yearParams, y=value, color=variable))+
 #   geom_line(aes(size=variable))+
   geom_line()+
